620d398943534c531234c6931ac95f5b8f0d3c4f
brndalx <brndalx@gmail.com>
2023-04-23 19:42:32 +0300

    feat:created files to handle model,auth,validation

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,3 +1,5 @@
+import express from "express";
+import usersController from "../controllers/usersController.js";
 // TO-DO: add auth middleware
 const router = express.Router();
 // For each route there is a call to the controller that implements the required logic
