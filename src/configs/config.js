0f6f59fadce38a56cf060b634faa5d2128a02384
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-04-29 21:17:41 +0300

    Revert "Merge pull request #9 from Okeanid-ISR/feature-swagger-doc-restaurant-client"
    This reverts commit 0226014864a2543872c44f109eb006fff8593ebb, reversing
    changes made to a2a9303a354d71ec87a9f12f514266232f56393a.

--- src/configs/config.js
+++
@@ -1,6 +0,0 @@
-// Initializing environment variables
-import { config } from "dotenv";
-config();
-export const urldb = process.env.URLDB;
-export const localurldb = process.env.LOCALURLDB;
-export const port = Number(process.env.PORT) || 3001;
--- src/routes/users.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import usersController from "../controllers/usersController.js";
-// TO-DO: add auth middleware
-const router = express.Router();
-// For each route there is a call to the controller that implements the required logic
-router.get("/", usersController.getUsers);
-
-export default router;
