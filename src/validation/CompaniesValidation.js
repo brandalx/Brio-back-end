dc6b24494732b4fdfb9449d951f9658a5c81ed40
brndalx <brndalx@gmail.com>
2023-05-05 13:06:14 +0300

    feat: companies validation finished

--- src/validation/CompaniesValidation.js
+++ src/validation/CompaniesValidation.js
@@ -1,13 +1,9 @@
 import Joi from "joi";
-
 // Joi validator for created schema
 export function validateCompanies(_reqBody) {
   let joiSchema = Joi.object({
-    title: Joi.string().min(2).max(150).required(),
-    address: Joi.string().min(2).max(150).required(),
-    location: Joi.string().min(2).max(150).required(),
-    image: Joi.string().min(1).max(150).allow(null, ""),
-    company: Joi.string().min(1).max(150).required(),
+    name: Joi.string().min(2).max(150).required(),
+    // todo: roles validation
   });
 
   return joiSchema.validate(_reqBody);
