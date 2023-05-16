af5aee73f66d59e42b8b8d0150a534b39d85e095
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-05-17 00:03:09 +0300

    feat: restaurant model updated + controller added + minor code refactoring

---
+++ src/models/restaurants.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const restaurantSchema = new mongoose.Schema({
+  id: { type: Number, unique: true },
+  title: String,
+  address: String,
+  location: String,
+  image: String,
+  reviews: Object,
+  company: { type: mongoose.Schema.Types.ObjectId, ref: "companies", unique: true },
+  products: [{ type: mongoose.Schema.Types.ObjectId, ref: "products" }]
+});
+
+const Restaurants = mongoose.model("Restaurants", restaurantSchema);
+
+export default Restaurants;
