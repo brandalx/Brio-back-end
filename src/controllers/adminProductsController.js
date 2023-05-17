0ab4709f2918641a952e21b85aaf9f0c8d210099
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-05-18 01:01:10 +0300

    feat: immense file deleted , routes configured

--- src/controllers/adminProductsController.js
+++ src/controllers/adminProductsController.js
@@ -1,4 +1,6 @@
 import {productsModel} from "../models/products.js";
+import {ordersModel} from "../models/orders.js";
+
 
 const adminProductsController = {
     async getProductById(req, res) {
@@ -13,9 +15,31 @@ const adminProductsController = {
             console.log(err);
             res.status(502).json({ err });
         }
+    },
+    async getAllOrders(req, res) {
+        try {
+            const orders = await ordersModel.find().populate('cartId');
+            res.json(orders);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({err});
+        }
+    },
+
+    async getOrderById(req, res) {
+        const {id} = req.params;
+
+        try {
+            const order = await ordersModel.findById(id).populate('cartId');
+            if (!order) {
+                return res.status(404).json({error: "Order not found"});
+            }
+            res.json(order);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({error: "Server error"});
+        }
     }
-
-
 };
 
 export default adminProductsController;
