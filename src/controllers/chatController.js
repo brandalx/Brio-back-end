dc24fd59ce603633d24b6d0b39f866e02fa83d58
brndalx <brndalx@gmail.com>
2023-05-18 04:07:52 +0300

    ferat: open ai api connection and basic request

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,11 +1,38 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  organization: "ORGANIZATION", //todo: pass organization key here after purchare
+  apiKey: "KEY",
+  //todo: pass api key here after purchare
+});
+
+const openai = new OpenAIApi(configuration);
+
 const chatController = {
   getChat(req, res) {
     try {
       res.json({ message: "Chat endpoint" });
     } catch (err) {
       console.log(err);
-      res.status(502).json({ err });
+      res.status(502).json({ error: err });
+    }
+  },
+
+  async postChat(req, res) {
+    try {
+      const chatResponse = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: req.body }],
+      });
+
+      console.log(chatResponse.data.choices[0].message.content);
+
+      res.json({ message: chatResponse.data.choices[0].message.content });
+    } catch (err) {
+      console.log(err);
+      res.status(502).json({ error: err });
     }
   },
 };
+
 export default chatController;
--- src/routes/chat.js
+++ src/routes/chat.js
@@ -2,5 +2,6 @@ import { Router } from "express";
 import chatController from "../controllers/chatController.js";
 const router = Router();
 router.get("/", chatController.getChat);
+router.post("/send", chatController.postChat);
 
 export default router;
