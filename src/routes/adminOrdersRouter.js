77863996a4c7eee062724c914d14f70c256bb4df
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-05-18 20:51:07 +0300

    WIX: routes fixes in process

---
+++ src/routes/adminOrdersRouter.js
@@ -0,0 +1,12 @@
+import express from "express";
+import adminOrdersController from "../controllers/adminOrdersController.js";
+
+const router = express.Router();
+
+router.get("/:id", adminOrdersController.getOrderById);
+router.get("/", adminOrdersController.getAllOrders);
+
+export default router;
+
+
+
