fd15fa8101f2c824670f04f385b5add02625a60d
brndalx <113251192+brandalx@users.noreply.github.com>
2023-05-21 05:45:48 +0300

    update: user model and validation

--- src/validation/userClientValidation.js
+++ src/validation/userClientValidation.js
@@ -6,21 +6,91 @@ export function validateUserClient(_reqBody) {
     lastname: Joi.string().min(2).max(150).required(),
     email: Joi.string().email().min(2).max(150).required(),
     birthdate: Joi.date().required(),
-    nickname: Joi.string().min(2).max(150).optional().required(),
+    nickname: Joi.string().min(2).max(150).optional().allow(null, ""),
     avatar: Joi.string().min(2).max(150).optional().allow(null, ""),
     password: Joi.string().min(2).max(150).required(),
-    address: Joi.object({
-      country: Joi.string().min(2).max(150).optional().allow(null, ""),
-      state: Joi.string().min(2).max(150).optional().allow(null, ""),
-      city: Joi.string().min(2).max(150).optional().allow(null, ""),
-      address1: Joi.string().min(2).max(150).optional().allow(null, ""),
-      address2: Joi.string().min(2).max(150).optional().allow(null, ""),
-    })
+    cart: Joi.array()
+      .items(
+        Joi.object({
+          productId: Joi.number().required(),
+          productAmount: Joi.number().required(),
+        })
+      )
       .optional()
-      .allow(null, ""),
+      .allow(null),
+    comments: Joi.array()
+      .items(
+        Joi.object({
+          comment: Joi.string().required(),
+          date: Joi.date().required(),
+        })
+      )
+      .optional()
+      .allow(null),
+    rate: Joi.array()
+      .items(
+        Joi.object({
+          blogId: Joi.number().required(),
+          rating: Joi.number().required(),
+        })
+      )
+      .optional()
+      .allow(null),
+    address: Joi.array()
+      .items(
+        Joi.object({
+          country: Joi.string().min(2).max(150).required(),
+          state: Joi.string().min(2).max(150).required(),
+          city: Joi.string().min(2).max(150).required(),
+          address1: Joi.string().min(2).max(150).required(),
+          address2: Joi.string().min(2).max(150).optional().allow(null, ""),
+        })
+      )
+      .optional()
+      .allow(null),
+    creditdata: Joi.array()
+      .items(
+        Joi.object({
+          paymentMethod: Joi.string().min(2).max(150).required(),
+          cardNumber: Joi.string().min(2).max(150).required(),
+          expirationDate: Joi.string().min(2).max(150).required(),
+          securityCode: Joi.string().min(2).max(150).required(),
+        })
+      )
+      .optional()
+      .allow(null),
+    orders: Joi.array()
+      .items(
+        Joi.object({
+          orderId: Joi.number().required(),
+          restaurant: Joi.string().min(2).max(150).required(),
+          creationDate: Joi.date().required(),
+          creationTime: Joi.string().min(2).max(150).required(),
+          status: Joi.string()
+            .valid("placed", "prepared", "out", "delivered", "cancelled")
+            .required(),
+          totalAmount: Joi.number().required(),
+          paymentSummary: Joi.object({
+            couponCode: Joi.string().min(2).max(150).optional().allow(null, ""),
+            tips: Joi.number().optional().allow(null),
+          }).optional(),
+        })
+      )
+      .optional()
+      .allow(null),
+    role: Joi.string().min(2).max(150).optional().default("USER"),
+    favorites: Joi.array()
+      .items(
+        Joi.object({
+          restaurant: Joi.string().min(2).max(150).required(),
+          dishes: Joi.array().items(Joi.number().required()).required(),
+        })
+      )
+      .optional()
+      .allow(null),
     phone: Joi.string().min(2).max(150).required(),
+    emailnotifications: Joi.boolean().optional(),
+    date_created: Joi.date().default(Date.now),
   });
   return joiSchema.validate(_reqBody);
 }
-
-//todo: correct validation according on future requests (in future releases)
