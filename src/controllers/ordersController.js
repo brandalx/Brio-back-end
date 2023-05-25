3f6658b80ce4b07ae0a5b812dd800196f92a5d71
brndalx <113251192+brandalx@users.noreply.github.com>
2023-05-26 02:07:21 +0300

    feat: user post new order

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -1,9 +1,74 @@
 import { ordersModel } from "../models/orders.js";
+import { UserClientModel } from "../models/userClient.js";
+import {
+  validateOrder,
+  validateUserOrder,
+} from "../validation/ordersValidation.js";
 const ordersController = {
   async getAllOrders(req, res) {
     try {
       let data = await ordersModel.find({});
       res.json(data);
+    } catch (err) {
+      console.log(err);
+      return res.status(502).json({ err });
+    }
+  },
+  async getOrdersById(req, res) {
+    let idParams = req.params.id;
+
+    try {
+      let data = await ordersModel.findById({ _id: idParams });
+      if (data) {
+        res.json(data);
+      } else {
+        res.status(404).json({ error: "Order not found" });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(502).json({ error: err });
+    }
+  },
+
+  async postOrder(req, res) {
+    const id = req.params.id;
+
+    try {
+      let user = await UserClientModel.findOne({ _id: id });
+      if (!user) {
+        return res.status(401).json({ err: "User not found" });
+      }
+
+      // todo: pass auth middleware
+
+      let validBodyClient = validateUserOrder(req.body);
+      if (validBodyClient.error) {
+        return res.status(400).json(validBodyClient.error.details);
+      }
+
+      const newOrder = {
+        restaurant: req.body.restaurant,
+        creationDate: new Date(),
+        creationTime: new Date(),
+        status: req.body.status,
+        paymentSummary: req.body.paymentSummary,
+      };
+
+      user.orders.push(newOrder);
+
+      await user.save();
+
+      const createdOrder = user.orders[user.orders.length - 1];
+
+      const orderId = createdOrder._id.toString(); // Convert objectId to string
+
+      const lastOrderIndex = user.orders.length - 1;
+      user.orders[lastOrderIndex].orderId = orderId;
+
+      await user.save();
+
+      console.log(user.orders);
+      res.status(201).json({ msg: true });
     } catch (err) {
       console.log(err);
       return res.status(502).json({ err });
---
+++ src/validation/ordersValidation.js
@@ -0,0 +1,37 @@
+import Joi from "joi";
+
+export function validateOrder(order) {
+  const schema = Joi.object({
+    restaurantRef: Joi.string().required(),
+    userRef: Joi.string().required(),
+    products: Joi.array()
+      .items(
+        Joi.object({
+          productRef: Joi.string().required(),
+          amount: Joi.number().integer().min(1).required(),
+        })
+      )
+      .required(),
+  });
+
+  return schema.validate(order);
+}
+
+export function validateUserOrder(order) {
+  const schema = Joi.object({
+    restaurant: Joi.string().min(2).max(150).required(),
+    status: Joi.string()
+      .valid("In progress", "Completed", "Canceled", "Suspended", "cancelled")
+      .required(),
+    paymentSummary: Joi.object({
+      couponCode: Joi.string().allow(null).allow("").min(2).max(150).optional(),
+      subtotal: Joi.number().required(),
+      tips: Joi.number().optional().allow(null),
+      shipping: Joi.number().required(),
+      discount: Joi.number().required(),
+      totalAmount: Joi.number().required(),
+    }).required(),
+  });
+
+  return schema.validate(order);
+}
