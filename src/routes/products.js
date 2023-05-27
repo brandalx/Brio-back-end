e668f097a385d4d549f80cb261e62cba5f18ccfa
brndalx <brndalx@gmail.com>
2023-05-28 00:05:56 +0300

    feat: added missed files

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,5 +3,6 @@ import productsController from "../controllers/productsController.js";
 
 const router = Router();
 router.get("/", productsController.getAllProducts);
+router.get("/:id", productsController.getProductById);
 
 export default router;
