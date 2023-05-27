9c785e252d9a504024aa97e2c673b638022189b7
brndalx <113251192+brandalx@users.noreply.github.com>
2023-05-27 17:23:00 +0300

    feat: user controller and user router for cart

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,7 @@ import usersController from "../controllers/usersController.js";
 // TO-DO: add auth middleware
 const router = express.Router();
 // For each route there is a call to the controller that implements the required logic
-router.get("/getAllUsers", usersController.getAllUsers);
+router.get("/", usersController.getUsers);
 router.get("/:id", usersController.getUserById);
 router.get("/:id/cart", usersController.getUserCart);
 router.get("/:id/address", usersController.getUserAddress);
@@ -13,5 +13,6 @@ router.get("/:id/orders", usersController.getUserOrders);
 router.put("/:id/putuserdata", usersController.putUserData);
 router.post("/:id/postuseraddress", usersController.postUserAddress);
 router.post("/:id/postusercard", usersController.postUserCard);
+router.post("/:id/posttocart", usersController.postToCart);
 
 export default router;
