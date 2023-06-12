f8a60a22505ff80cd7b8127827979accfec0e661
brndalx <113251192+brandalx@users.noreply.github.com>
2023-06-12 17:09:52 +0300

    feat: json web token added and envs consts added

--- src/configs/config.js
+++ src/configs/config.js
@@ -4,3 +4,5 @@ config();
 export const urldb = process.env.URLDB;
 export const localurldb = process.env.LOCALURLDB;
 export const port = Number(process.env.PORT) || 3001;
+export const tokenSecret1 = process.env.TOKENSECRET1;
+export const tokenSecret2 = process.env.TOKENSECRET1;
