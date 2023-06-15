25357964d48a36171b1db28b980ddffa0a2c0032
brndalx <113251192+brandalx@users.noreply.github.com>
2023-06-15 04:34:36 +0300

    feat: admin auth basic middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -17,4 +17,30 @@ const auth = (req, res, next) => {
   }
 };
 
+const authAdmin = (req, res, next) => {
+  // Checking if a token is sent in the header
+  let token = req.header("x-api-key");
+  if (!token) {
+    return res
+      .status(401)
+      .json({ err: "You must send a token to this endpoint" });
+  }
+  try {
+    let decodeToken = jwt.verify(token, config.tokenSecret);
+    // Checking if the user is an admin
+    if (decodeToken.role != "ADMIN") {
+      return res
+        .status(401)
+        .json({ err: "You must send an admin token to this endpoint" });
+    }
+    // req -> parameter object shared among all functions in the router chain
+    req.tokenData = decodeToken;
+    // Proceeding to the next function in the router chain
+    // next() -> calls the next function in the router chain
+    next();
+  } catch (err) {
+    res.status(401).json({ err: "Token invalid or expired" });
+  }
+};
+
 export default auth;
