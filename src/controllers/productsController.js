0a4fc05c55075cf28838cedd560824731c9e1203
brndalx <113251192+brandalx@users.noreply.github.com>
2023-07-10 02:37:01 +0300

    feat: limited amunt of products, search

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -9,6 +9,45 @@ const productsController = {
       return res.status(502).json({ err });
     }
   },
+
+  async getLimitedProducts(req, res) {
+    let perPage = 5;
+
+    let page = req.query.page - 1 || 0;
+
+    let sort = req.query.sort || "_id";
+
+    let desc = req.query.desc == "yes" ? 1 : -1;
+    try {
+      let data = await productsModel
+        .find({})
+        .limit(perPage)
+        .skip(page * perPage)
+        .sort({ [sort]: desc });
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+      return res.status(502).json({ err });
+    }
+  },
+
+  async getSearchProducts(req, res) {
+    let querySearch = req.query.s;
+    let searchExpression = new RegExp(querySearch, "i");
+    try {
+      let data = await productsModel
+        .find({
+          $or: [{ title: searchExpression }, { description: searchExpression }],
+        })
+        .limit(20);
+
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+      return res.status(502).json({ err });
+    }
+  },
+
   async getProductById(req, res) {
     let idParams = req.params.id;
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,5 +4,7 @@ import productsController from "../controllers/productsController.js";
 const router = Router();
 router.get("/", productsController.getAllProducts);
 router.get("/:id", productsController.getProductById);
+router.get("/limit", productsController.getLimitedProducts);
+router.get("/search", productsController.getSearchProducts);
 
 export default router;
