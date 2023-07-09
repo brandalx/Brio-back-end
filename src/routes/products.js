1ef4af9ce31f96008de32f0c2d6ab8438ca1855b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-07-10 02:41:56 +0300

    fix: paths

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,7 +4,7 @@ import productsController from "../controllers/productsController.js";
 const router = Router();
 router.get("/", productsController.getAllProducts);
 router.get("/:id", productsController.getProductById);
-router.get("/limit", productsController.getLimitedProducts);
-router.get("/search", productsController.getSearchProducts);
+router.get("/single/limit", productsController.getLimitedProducts);
+router.get("/single/search", productsController.getSearchProducts);
 
 export default router;
