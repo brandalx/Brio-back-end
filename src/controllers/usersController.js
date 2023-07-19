310d8abceaa53e15af62574d43dd769c1124b410
brndalx <113251192+brandalx@users.noreply.github.com>
2023-07-19 14:22:46 +0300

    feat: image changed

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1199,7 +1199,7 @@ const usersController = {
     <tbody>
       <tr>
         <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
-          <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:100% !important; width:100%; height:auto !important;" width="700" alt="" data-proportionally-constrained="true" data-responsive="true" src="http://cdn.mcauto-images-production.sendgrid.net/27548861a3bba7f7/5a35075c-8fbe-430b-8354-6b7e0293f36f/1920x1146.jpg">
+          <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:100% !important; width:100%; height:auto !important;" width="700" alt="" data-proportionally-constrained="true" data-responsive="true" src="http://cdn.mcauto-images-production.sendgrid.net/27548861a3bba7f7/7a90bd88-1f04-448b-b2af-19f94a862f29/1920x1146.jpg">
         </td>
       </tr>
     </tbody>
