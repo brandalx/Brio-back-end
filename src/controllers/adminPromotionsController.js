5d7c1efa8e31234e007a20d8d21319ba88c8296c
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-07-22 01:33:48 +0300

    feat: delete promotion func added

--- src/controllers/adminPromotionsController.js
+++ src/controllers/adminPromotionsController.js
@@ -42,6 +42,22 @@ const promotionsController = {
       res.status(502).json({ error: err });
     }
   },
+  async deletePromotion(req, res) {
+    try {
+      const deletedPromotion = await promotionsModel.findByIdAndRemove(
+        req.params.id
+      );
+
+      if (!deletedPromotion) {
+        return res.status(404).json({ message: "Promotion not found" });
+      }
+
+      res.json({ message: "Promotion deleted", deletedPromotion });
+    } catch (err) {
+      console.error(err);
+      res.status(502).json({ error: err });
+    }
+  },
 };
 
 export default promotionsController;
